15752b30e63fad80e41cb7ac3349001d193b145f
zjt <zhangjiantong@ybm100.com>
2020-02-28 22:47:26 +0800

    //first commit

--- src/components/index.js
+++ src/components/index.js
@@ -13,9 +13,8 @@ function App() {
                     defaultSelectedKeys={['2']}
                     style={{ lineHeight: '64px' }}
                 >
-                    <Menu.Item key="1">nav 1</Menu.Item>
-                    <Menu.Item key="2">nav 2</Menu.Item>
-                    <Menu.Item key="3">nav 3</Menu.Item>
+                    <Menu.Item key="1">首页</Menu.Item>
+                    <Menu.Item key="2">活动页</Menu.Item>
                 </Menu>
             </Header>
             <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
